7775f182080905b54b83d0b1ecde236150b559d4
Robert Herschel Hawk <robert@the-hawk.us>
2018-10-18 04:15:22 -0700

    Added wrapper script
    Added a wrapper script for automatically sourcing the conda environment
    (now stored in the environment.yml file).

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # MakeDoc
 A makefile for creating different kinds of documents. Currently being
 rewritten.
+
+# Installation
+~~~
+conda env create environment.yml
+ln -s ./makedoc.sh ~/bin/makedoc
+~~~
---
+++ makedoc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Solution from: https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+
+source activate makedoc
+python $DIR/main.py "$@"
+source deactivate
